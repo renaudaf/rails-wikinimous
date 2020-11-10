2a453de8b8aa35de37b8c06ff31fe13e6965076d
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-10 16:08:58 -0500

    every methods and link works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,21 +9,33 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
  end
 
  def new
+  @article = Article.new
  end
 
  def create
+  @article = Article.new(article_params)
+  @article.save
+  redirect_to articles_path
  end
 
  def edit
  end
 
  def update
+  @article.update(article_params)
+  redirect_to articles_path
  end
 
  def destroy
+  @article.destroy
+  redirect_to articles_path
  end
 
  def set_article
   @article = Article.find(params[:id])
  end
+
+ def article_params
+  params.require(:article).permit(:title, :content)
+end
 end
