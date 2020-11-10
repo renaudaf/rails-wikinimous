3baae78a061f5a8226d82a2d31bb5b0cc058561b
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-10 15:26:15 -0500

    models and seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+ Article.create!({
+  title:Faker::Dessert.flavor,
+  content: Faker::ChuckNorris.fact,
+})
+end
