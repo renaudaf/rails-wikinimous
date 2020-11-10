a51566828aa3946aeab795354fb83ed15213aa20
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-10 15:50:58 -0500

    index and show works

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+ def index
+  @articles = Article.all
+ end
+
+ def show
+ end
+
+ def new
+ end
+
+ def create
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+
+ def set_article
+  @article = Article.find(params[:id])
+ end
+end
